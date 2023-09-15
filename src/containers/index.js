489985fe1aa158796f79ace4e47a08f52ebe7f9e
kiranraoboinapally <kiranraoboinapally@gmail.com>
2023-09-15 11:50:45 +0530

    third with no erorr

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,5 +2,5 @@ export { default as Blog} from './blog/Blog';
 export { default as Features} from './features/Features';
 export { default as Footer} from './footer/Footer';
 export { default as Header} from './header/Header';
-export { default as Possibililty} from './possibility/Possibililty';
+export { default as Possible} from './possible/Possible';
 export { default as whatDepressionHUB} from './whatDepressionHUB/whatDepressionHUB';
