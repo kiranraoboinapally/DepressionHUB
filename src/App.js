fa14990f7ea159d963536233e1f99ab7eaf171a3
kiranraoboinapally <kiranraoboinapally@gmail.com>
2023-09-19 11:05:15 +0530

    next

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Footer , Blog ,Possible,Features,whatDepressionHUB,Header} from './containers';
 import { CTA,Article,Brand,Feature,Navbar} from './components';
+import './App.css';
 const App = () => {
   return (
     <div className="App">
       <div className="gradient__bg">
         <Navbar />
+        <div className="gradient__bg2">
         <Header />
-      </div>
-      <Brand />
+        <Brand />
       <whatDepressionHUB />
       <Features />
       <Possible />
       <CTA />
       <Blog />
       <Footer />
+      </div>
+    </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './navbar.css';
+import DepressionHUB from '../../assets/DepressionHUB.png';
+
+import {RiMenu3Line,RiCloseLine} from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="dhubb__navbar">
+      <div className="dhubb__navbar-links">
+        <div className="dhubb__navbar-links_logo">
+          <img  src={DepressionHUB} alt="DepressionHUB" />
+          </div>
+         <div className="dhubb__navbar-links_containers">
+         <div className="dhubb__navbar-menu_containers"> 
+         <div className="dhubb__navbar-sign">
+         <p><a href="#Blog">Blog</a></p>
+         <p><a href="#Possible">Lang</a></p>
+         <p><a href="#Features">Login</a></p>
+
+         </div>
+         </div>
+         </div>
+    </div>
+    </div>
   )
 }
-
 export default Navbar
