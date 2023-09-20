67c03f990c3f8b4d53402c60f3e84c1ad81fe8bd
kiranraoboinapally <kiranraoboinapally@gmail.com>
2023-09-20 11:39:33 +0530

    mobileview in header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 import DepressionHUB from '../../assets/DepressionHUB.png';
 
 import {RiMenu3Line,RiCloseLine} from 'react-icons/ri';
 
+const Menu = () => {
+  <>
+  <p><a href="#Blog">Blog</a></p>
+  <p><a href="#Possible">Lang</a></p>
+  <p><a href="#Features">Talk to Therapist</a></p>
+  </>
+
+}
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="dhubb__navbar">
       <div className="dhubb__navbar-links">
@@ -18,7 +28,18 @@ const Navbar = () => {
          </div>
          <div className="dhubb__navbar-sign">
           <button type="button">Login</button></div>
-         </div>
+         </div><div className="dhubb__navbar-menu">
+          {toggleMenu
+          ?<RiCloseLine color="fff" size={27} onClick={()=> setToggleMenu(false)}/>
+          :<RiMenu3Line color="fff" size={27} onClick={()=> setToggleMenu(true)}/>
+        }
+        {toggleMenu && (
+          <div className="dhubb__navbar-menu_container scale-up-center">
+          <div className="dhubb__navbar-menu_container-links"><Menu />
+          </div>
+          </div>
+        )}
+    </div>
     </div>
   )
 }
